040d12066eb0e3ff1de51cc4fb8c1524c5601e92
DeborahImj <deborahimajieng@gmail.com>
2026-09-23 16:34:34 +0100

    Built mobile nav burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
 
+// MOBILE NAVBAR
+
+const navBurger = document.querySelector('#navBurger');
+const navMenu = document.querySelector('#navMenu');
+
+navBurger.addEventListener('click', toggleNavMenu);
+
+function toggleNavMenu() {
+  navBurger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
